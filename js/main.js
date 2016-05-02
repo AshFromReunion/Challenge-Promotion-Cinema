2401640c8670476402fb23adca3dae8e7009f165
Mathieu JULIE <mathieujulie974@hotmail.com>
2016-05-02 11:28:58 +0400

    Menu accordéon

--- js/main.js
+++ js/main.js
@@ -2,10 +2,39 @@
 
 $(function () {
     $("#accordion").accordion({
-        collapsible: true
+        collapsible: true,
+        active : false,
+        
     });
 });
 
 //DATE PICKER
 
 $('.datepicker').datepicker({}); 
+
+
+
+
+
+// FIXER LE MENU ACCORDEON
+var positionElementInPage = $('#accordion').offset().top;
+$( window ).resize(function() {
+    positionElementInPage = $('#accordion').offset().top;
+});
+$(window).scroll(
+    function() {
+        if ($(window).scrollTop() > positionElementInPage && $(window).width() > 992) {
+            // fixed
+            $('#accordion').addClass("fixed");
+            $('#liste-film').addClass("col-md-offset-2");
+           
+            
+        } else {
+            // unfixed
+            $('#accordion').removeClass("fixed");
+             $('#liste-film').removeClass("col-md-offset-2");
+        }
+    }
+ 
+  );
+
